bca9ad1ab3061c58e61ee9f4e70626a351004edb
krzy-sok <01178595@pw.edu.pl>
2025-08-20 21:50:27 +0200

    feat: header files for host and ping_row

---
+++ arp-test/src/classes/host.hpp
@@ -0,0 +1,13 @@
+#include <string>
+
+class Host
+{
+    private:
+        string ip;
+        string mac;
+        stirng interface;
+
+    public:
+        Host(string ip, string mac, string interface)
+        PingRow ping() {}
+}
---
+++ arp-test/src/classes/ping_row.hpp
@@ -0,0 +1,14 @@
+#include <string>
+
+class PingRow
+{
+    private:
+        string ip;
+        string mac;
+        stirng interface;
+        int rtt;
+        int timestamp;
+
+    public:
+        PingRow(string ip, string mac, stirng interface, int rtt, int timestamp){}
+}
